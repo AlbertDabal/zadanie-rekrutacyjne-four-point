9f0d76b80ba52a9ff8163e77de7475bc4ab95151
Albert Dąbal <60232394+AlbertDabal@users.noreply.github.com>
2022-04-26 15:55:28 +0200

    Add component Maps

--- src/components/organisms/maps/Maps.tsx
+++ src/components/organisms/maps/Maps.tsx
@@ -7,46 +7,86 @@ import satelite from "images/maps/satelite.png";
 import waterline from "images/maps/waterline.png";
 
 const Wrapper = styled.div`
-  width: 80%;
-  max-width: 1280px;
   min-height: 100vh;
-
+  width: 80%;
   padding-top: 16vh;
 
   @media (min-width: 500px) and (max-width: 1024px) {
     padding-top: 8vh;
+    width: 85%;
   }
 
   @media (min-width: 1025px) {
     padding-top: 2vh;
+    width: 80%;
+    max-width: 1280px;
   }
 `;
 
 const Map = styled.img`
   width: 100%;
-  height: 40vh;
+  height: 70vh;
+  margin-bottom: 10vh;
+  margin-top: 2vh;
+
+  @media (min-width: 500px) and (max-width: 1024px) {
+    height: 45vh;
+    margin-bottom: 8vh;
+    margin-top: 2vh;
+  }
+
+  @media (min-width: 1025px) {
+    height: 50vh;
+    margin-bottom: 10vh;
+    margin-top: 0vh;
+  }
+`;
+
+const Navigation = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  margin-bottom: 2vh;
+  margin-top: 3vh;
+
+  @media (min-width: 500px) and (max-width: 1024px) {
+    padding-top: 1vh;
+    flex-direction: row;
+    justify-content: flex-end;
+  }
+
+  @media (min-width: 1025px) {
+    flex-direction: row;
+    justify-content: flex-end;
+    margin-bottom: 3vh;
+    margin-top: 0;
+  }
 `;
 
 export const Maps = () => {
   const [option, setOption] = useState(["Satelite view", "Waterline"]);
   const [selectItem, setSelectItem] = useState("Satelite view");
 
-  useEffect(() => {}, [option]);
-
   return (
     <Wrapper>
       <Heading>Water Area Detection</Heading>
       <Line />
-      <Select
-        setSelectItem={setSelectItem}
-        option={option}
-        setOption={setOption}
-      />
-      {option[0] === "Satelite view" ? (
+      <Navigation>
+        <Select
+          setSelectItem={setSelectItem}
+          option={option}
+          setOption={setOption}
+        />
+      </Navigation>
+      {selectItem === "Satelite view" ? (
         <Map src={satelite} />
       ) : (
         <Map src={waterline} />
       )}
+
+      <Heading>ChlorophYll</Heading>
+      <Line />
       <Map src={satelite}></Map>
     </Wrapper>
   );
